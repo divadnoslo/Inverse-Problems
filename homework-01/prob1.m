15a4fdce0105bebd18a7a0f2d9eeebf4a4783bf7
David <divadnoslo@gmail.com>
2025-02-01 10:50:05 -0700

    created a LaTeX template for homework problems

--- homework-01/prob1.m
+++ homework-01/prob1.m
@@ -4,26 +4,33 @@ clc
 
 %% Problem 01
 
+% Discritize the integral into 20 equally spaced points
 n = 20;
-dx = 1/n;
+dz = 1/n;
+z = 0 : dz : 1;
+
+% Select values of x
+nx = 30;  % just picking something different than 20 for now
+dx = 1/nx;
 x = 0 : dx : 1;
 
-kernal = @(x, z)(5.*sin( x .* z));
-dataModel = @(x)(50.*sin(x) - 50.*sin(x).*cos(x));
-trueModel = @(x)(10.*x.*sin(x));
+% Create Function Handles
+kernal = @(x, z)(5.*sin(x.*z));
+m_true = @(x)(10.*x.*sin(x));
+fredholm_inside = @(x, z)(kernal(x, z) * m_true(x));
+fredhom_ifk = @(x, z)(integral(fredholm_inside(x, z), 0, 1));
 
 
 
 
-
-% Plot Results
-fig = figure("Name", "IFK vs. True Model");
-hold on
-plot(x, trueModel(x), 'k', "LineWidth", 2)
-plot(x, dataModel(x), 'r.', 'MarkerSize', 5)
-title("IFK vs. True Model")
-xlabel("x")
-ylabel("m_t_r_u_e(x)")
-grid on
-grid minor
-hold off
+% % Plot Results
+% fig = figure("Name", "IFK vs. True Model");
+% hold on
+% plot(x, trueModel(x), 'k', "LineWidth", 2)
+% plot(x, d_true(x), 'r.', 'MarkerSize', 5)
+% title("IFK vs. True Model")
+% xlabel("x")
+% ylabel("m_t_r_u_e(x)")
+% grid on
+% grid minor
+% hold off
